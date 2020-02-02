85ac3b23ca4cd821c2875787e5d1e69d5e997288
Yunsang Jeong <kunka8271@digger.works>
2020-02-03 04:17:02 +0900

    complete

--- lib/python_builder.ts
+++ lib/python_builder.ts
@@ -1,13 +1,29 @@
 import * as child from 'child_process'
 
+export var lastOutPut : string;
+export var isComplete : boolean = false;
+
+export function getOutPut() : string{
+    if(isComplete == true){
+        isComplete = false;
+        return lastOutPut;
+    }
+    return "Not Complete";
+}
+
 export function runPython(path: string, file : string){
+    isComplete = false;
     var _file : string[] = file.split(".");
 
     var command = path + ' ' + file;
-
     if(_file[_file.length - 1] === "py"){
         var foo: child.ChildProcess = child.exec(command, function(err, stdout, stderr){
-            console.log(stdout) 
+            if(err){
+                throw new Error("Error" + err.message);
+            }
+            
+            lastOutPut = stdout;
+            isComplete = true;
         })
     }
     else{
--- test/test.js
+++ test/test.js
@@ -3,24 +3,12 @@ var expect = require('chai').expect;
 var index = require('../dist/python_builder.js');
 
 describe('runPython function test', () => {
-  it('should return Boys', () => {
-    var result = index.runPython('Boy',"adsf.py");
-    expect(result).to.equal('Boys');
+  it('return Hello', () => {
+    var result = index.runPython('C:/Users/Yunsang/Anaconda3/envs/test/python.exe',"C:/Users/Yunsang/Documents/test.py");
+    setTimeout(function(){
+      var output = index.getOutPut();
+      console.log(output);
+    }, 500);
+    // expect(output).to.equal("Hello");
   })
-  it('should return Girls', () => {
-      var result = index.runPython('Girl',"asdf");
-      expect(result).to.equal('Girls');
-  });
-  it('should return Geese', () => {
-      var result = index.runPython('Goose',"asdf");
-      expect(result).to.equal('Geese');
-  });
-  it('should return Toys', () => {
-      var result = index.runPython('Toy',"asdf");
-      expect(result).to.equal('Toys');
-  });
-  it('should return Men', () => {
-      var result = index.runPython('Men',"asdf.py");
-      expect(result).to.equal('Men');
-  });
 })
